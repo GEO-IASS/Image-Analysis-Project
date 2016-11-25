5561ff16cbff23eda4ef0689bea5a35b676fa856
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-25 15:12:58 +0530

    Properly added arguments in function

--- partition_data.m
+++ partition_data.m
@@ -1,11 +1,7 @@
-function [tr_set,test_set] = fetch_files(dsObj)
-% as of 16a, bagOfFeatures still requires an imageSet object to run. This
-% is on the roadmap to change in the future, but for now, we need to
-% convert this to an imageSet object! 
+function [training_set,test_set] = partition_data(dsObj, m, n)
 
-image_location = fileparts(dsObj.Files{1});
-
-imset = imageSet(strcat(image_location,'\..'),'recursive');
-[tr_set,test_set] = imset.partition(700);
-test_set = test_set.partition(200);
+    image_location = fileparts(dsObj.Files{1}); 
+    imset = imageSet(strcat(image_location,'\..'),'recursive');
+    [training_set,test_set] = imset.partition(m);
+    test_set = test_set.partition(n);
 end
