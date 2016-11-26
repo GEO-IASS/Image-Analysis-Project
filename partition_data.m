2efb44f8b0952de4d6d2411579b6c482fe7786d7
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-26 14:35:15 +0530

    changes done according to linux diretory structure

--- partition_data.m
+++ partition_data.m
@@ -1,7 +1,9 @@
 function [training_set,test_set] = partition_data(dsObj, m, n)
 
     image_location = fileparts(dsObj.Files{1}); 
-    imset = imageSet(strcat(image_location,'\..'),'recursive');
+    disp(image_location);
+    imset = imageSet(strcat(image_location,'/..'),'recursive');
+    disp(imset);
     [training_set,test_set] = imset.partition(m);
     test_set = test_set.partition(n);
 end
