a7a91c0e2ffe8fd1c4e33687a87dfc2a01d03ec2
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-30 21:17:26 +0530

    really final commit

--- get_random_files.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = get_random_files( input_args )
-%GET_RANDOM_FILES Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- main.m
+++ main.m
@@ -2,6 +2,7 @@
 myfolder = './Dataset';
 imds = imageDatastore(myfolder, 'IncludeSubfolders',true,'LabelSource','foldernames');
 tbl = countEachLabel(imds);
+minSetCount = min(tbl{:,2});
 %% To Display Montage of few data
 %sample = splitEachLabel(imds,8);
 %montage(sample.Files(1:20));
@@ -10,10 +11,10 @@ tbl = countEachLabel(imds);
 %% Partition data set into 2 parts
 %% Training set and test set
 % Macros for training set and test set
-TRAINING_SET_SIZE = 99;
-TEST_SET_SIZE = 1;
+TRAINING_SET_SIZE = 2;
+TEST_SET_SIZE = 2;
 [training_set, test_set] = partition_data(imds, TRAINING_SET_SIZE, TEST_SET_SIZE);
-%% Extract features from training set images 
+% Extract features from training set images 
 tic
     bag = bagOfFeatures(training_set, 'VocabularySize', 250, 'PointSelection', 'Detector');
     fruitsdata = double(encode(bag, training_set));
@@ -30,7 +31,7 @@ disp(randi(training_set(1).Count));
 %title('Visual Word Occurrences');
 %xlabel('Visual Word Index');
 %ylabel('Frequency');
-%% 
+% 
 fruitsImageData = array2table(fruitsdata);
 replicated_elem = repelem({training_set.Description}', [training_set.Count], 1);
 fruitType = categorical(replicated_elem);
@@ -38,22 +39,32 @@ fruitsImageData.fruitType = fruitType;
 
 %% Test out accuracy on test set!
 
-[trainedClassifier, ~] = get_classifier(fruitsImageData);
+[trainedClassifier, ~] = random(fruitsImageData);
 testFruitsData = double(encode(bag, test_set));
 testFruitsData = array2table(testFruitsData,'VariableNames',trainedClassifier.RequiredVariables);
 actualFruitType = categorical(repelem({test_set.Description}', [test_set.Count], 1));
 predictedOutcome = trainedClassifier.predictFcn(testFruitsData);
 correctPredictions = (predictedOutcome == actualFruitType);
 validationAccuracy = sum(correctPredictions)/length(predictedOutcome);
-
+disp(validationAccuracy);
 %% Get File From Our User and show classifier best Guess
 warning('off', 'Images:initSize:adjustingMag');
 user_input_file = imgetfile;
 % ii = randi(size(test_set,2));
-% jj = randi(test_set(ii).Count);
+% jj = randi(test_set(ii).Count);4
 img = imread(user_input_file);
 imshow(img)
 imagefeatures = double(encode(bag, img));
 % Find two closest matches for each feature
-[bestGuess, score] = predict(trainedClassifier.ClassificationEnsemble,imagefeatures);
+[bestGuess, score] = predict(trainedClassifier.ClassificationSVM,imagefeatures);
+title(sprintf('Best Guess: %s;',char(bestGuess)));
+
+%%
+a = [2 4 3 6];
+c = csvread('test.csv');
+s = struct('v', c);
+% r = readtable('test.csv');
+% s = table2struct(r);
+% % % load('go');
+[bestGuess, score] = predict(trainedClassifier.ClassificationTree,s.v);
 title(sprintf('Best Guess: %s;',char(bestGuess)));
